2c366dbc40fe234bda7234a8ce4ffeae4d36c4ac
Ashley Connor <ash.connor@gmail.com>
2020-11-14 12:51:30 -0500

    Add models to admin, set plural names

--- matching/admin.py
+++ matching/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Cohort
+from .models import Cohort, Mentor, Mentee, Match, Settings
 
 admin.site.register(Cohort)
+admin.site.register(Mentor)
+admin.site.register(Mentee)
+admin.site.register(Match)
+admin.site.register(Settings)
