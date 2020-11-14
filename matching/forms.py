dd6f77081550780ff316203b98d39517e8690e4b
Sandy Dai <sandydai@hotmail.com>
2020-11-14 17:27:06 -0500

    Clean up forms.py

--- matching/forms.py
+++ matching/forms.py
@@ -1,14 +1,8 @@
 from django import forms
-from .models import Mentee, Document
+from .models import Document
 
 class UploadFileForm(forms.Form):
     model = Document
     title = forms.CharField(max_length=50)
     file = forms.FileField()
 
-class ModelUploadFileForm(forms.ModelForm):
-    class Meta:
-
-        model = Mentee
-
-        fields = ['FName', 'LName']
