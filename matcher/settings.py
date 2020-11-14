9635024f4a3fddf40fc0903ca2a6cd19f159c219
kramarv <kramarv@yahoo.com>
2020-11-13 22:28:42 -0500

    add simple templates to password reset, login,logout

--- matcher/settings.py
+++ matcher/settings.py
@@ -109,7 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# TO-DO: enhance redirect and reset link should be taken from logs
 LOGIN_REDIRECT_URL = '/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
