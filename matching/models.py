977f7b16098bbe43c4b221a8fb6df35d6028e98a
Ashley Connor <ash.connor@gmail.com>
2020-11-14 12:34:00 -0500

    Add basic models for all entities

--- matching/models.py
+++ matching/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Cohort(models.Model):
@@ -8,3 +9,37 @@ class Cohort(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Mentor(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Mentee(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Match(models.Model):
+    APPROVED = "AP"
+    PENDING = "PD"
+
+    STATUS_CHOICES = [(APPROVED, "Approved"), (PENDING, "Pending")]
+
+    cohort = models.ForeignKey(Cohort, on_delete=models.CASCADE)
+    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE)
+    mentee = models.ForeignKey(Mentee, on_delete=models.CASCADE)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=PENDING)
+    approver = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Match(Mentor={self.mentor}, Mentee={self.mentee})"
+
+
+class Settings(models.Model):
+    cohort = models.ForeignKey(Cohort, on_delete=models.CASCADE)
