83830b0d6ff228304495057a41d736d24d46bae5
Ashley Connor <ash.connor@gmail.com>
2020-11-14 11:12:39 -0500

    Formatting

--- matcher/views.py
+++ matcher/views.py
@@ -1,17 +1,15 @@
 from django.contrib.auth import authenticate, login, logout
-def login_view(request):
-  username = request.POST['username']
-  password = request.POST['password']
-  user = authenticate(request, username=username, password=password)
+
+def login(request):
+  user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+
   if user is not None:
     login(request, user)
     # Redirect to a success page.
-      ...
   else:
      # Return an 'invalid login' error message.
-    ...
-  from django.contrib.auth import logout
+    pass
 
-def logout_view(request):
-  logout(request)
-# Redirect to a success page.
+def logout(request):
+    logout(request)
+    # Redirect to a success page.
