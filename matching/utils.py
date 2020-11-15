09c112e5f9e8092a5a93646b2e4a621be189b174
kramarv <kramarv@yahoo.com>
2020-11-14 23:03:14 -0500

    minor refactoring

--- matching/utils.py
+++ matching/utils.py
@@ -7,19 +7,20 @@ def handle_upload_files(f):
 
     for chunk in f.chunks():
         print(chunk)
-        for row in chunk:
-            print(row)
 
-        reader = csv.DictReader(open('mentor.csv', 'r'))
-        dict_list = []
-        for line in reader:
-            dict_list.append(line)
+def handle_file(variables_file):
 
+    # Open variable-based csv, iterate over the rows and map values to a list of dictionaries containing key/value pairs
 
-        for lline in dict_list:
-            mentor = Mentor(name=lline["First Name"] + " " + lline["Last Name"], email=lline["Personal Email Address"], other_attributes={})
-            mentor.save()
+    reader = csv.DictReader(open(variables_file, 'r'))
+    dict_list = []
+    for line in reader:
+        dict_list.append(line)
 
+    for dict_line in dict_list:
+        print(dict_line["Personal Email Address"])
+        mentor = Mentor(name=dict_line["First Name"] + " " + dict_line["Last Name"], email=dict_line["Personal Email Address"], other_attributes={})
+        mentor.save()
 
 
 def handle_mentee_files(f):
@@ -28,6 +29,7 @@ def handle_mentee_files(f):
 
 def handle_mentor_files(f):
     handle_upload_files(f)
+    handle_file("mentor.csv")
 
 
 def handle_return_mentor_files(f):
