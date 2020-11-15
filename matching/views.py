e1ae5cdd4c9143cd3524e77f2017f2c8f98e5b1d
Sandy Dai <sandydai@hotmail.com>
2020-11-15 16:06:56 -0500

    Show data based on cohort

--- matching/views.py
+++ matching/views.py
@@ -15,14 +15,21 @@ def index(request):
 @login_required
 def show(request, cohort_id):
     cohort = get_object_or_404(Cohort, pk=cohort_id)
+    request.session['cohort_id'] = cohort_id
     return render(request, "cohorts/show.html", {"cohort": cohort})
 
 @login_required
 def show_matching1(request): 
-    #TODO:need cohort ID to get exact subset?
-    mentee_list = Mentee.objects.all()
-    mentor_list = Mentor.objects.all()
-    context_dict = {'Mentees': mentee_list, 'Mentors':mentor_list, 'test': [1,2,3]}
+    if 'cohort_id' in request.session:
+        cohort_id = request.session['cohort_id']
+        mentee_list = Mentee.objects.get(cohort_id=cohort_id)
+        mentor_list = Mentor.objects.get(cohort_id=cohort_id)
+    else: 
+        messages.warning(request, 'To narrow your findings, please select a cohort from the main page')
+        mentee_list = Mentee.objects.all()
+        mentor_list = Mentor.objects.all()
+
+    context_dict = {'Mentees': mentee_list, 'Mentors':mentor_list}
     return render(request, "match/match1.html", {"context_dict":context_dict})
 
 @login_required
