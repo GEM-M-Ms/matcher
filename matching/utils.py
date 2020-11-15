20d82b604170b9838f429ab7e87d6f0cb48245a6
kramarv <kramarv@yahoo.com>
2020-11-14 22:57:45 -0500

    hardcoded file

--- matching/utils.py
+++ matching/utils.py
@@ -1,10 +1,25 @@
 import csv
+import json
+from .models import Mentor
 
 
 def handle_upload_files(f):
 
     for chunk in f.chunks():
         print(chunk)
+        for row in chunk:
+            print(row)
+
+        reader = csv.DictReader(open('mentor.csv', 'r'))
+        dict_list = []
+        for line in reader:
+            dict_list.append(line)
+
+
+        for lline in dict_list:
+            mentor = Mentor(name=lline["First Name"] + " " + lline["Last Name"], email=lline["Personal Email Address"], other_attributes={})
+            mentor.save()
+
 
 
 def handle_mentee_files(f):
