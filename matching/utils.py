e705fcd4799ab332c91fa94dcf6855fe4d76112d
Ashley Connor <ash.connor@gmail.com>
2020-11-15 00:16:36 -0500

    Convert uploaded file to dict

--- matching/utils.py
+++ matching/utils.py
@@ -1,19 +1,21 @@
 import csv
+import io
+
+def convert_to_dicts(uploaded_file):
+    file_bytes = uploaded_file.read().decode('utf-8')
+    reader = csv.DictReader(io.StringIO(file_bytes))
+    return [line for line in reader]
+
+def handle_mentee_files(uploaded_file):
+    data = convert_to_dicts(uploaded_file)
+    print(data)
 
 
-def handle_upload_files(f):
-
-    for chunk in f.chunks():
-        print(chunk)
-
-
-def handle_mentee_files(f):
-    handle_upload_files(f)
+def handle_mentor_files(uploaded_file):
+    convert_to_dicts(uploaded_file)
+    print(data)
 
 
-def handle_mentor_files(f):
-    handle_upload_files(f)
-
-
-def handle_return_mentor_files(f):
-    handle_upload_files(f)
+def handle_return_mentor_files(uploaded_file):
+    convert_to_dicts(uploaded_file)
+    print(data)
