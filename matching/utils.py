210399ec42b0f9bafeb7f114fd19c6a2a9493677
kramarv <kramarv@yahoo.com>
2020-11-15 00:43:49 -0500

    save object

--- matching/utils.py
+++ matching/utils.py
@@ -1,5 +1,7 @@
 import csv
 import io
+import json
+from .models import Mentor
 
 def convert_to_dicts(uploaded_file):
     file_bytes = uploaded_file.read().decode('utf-8')
@@ -12,10 +14,15 @@ def handle_mentee_files(uploaded_file):
 
 
 def handle_mentor_files(uploaded_file):
-    convert_to_dicts(uploaded_file)
+    data = convert_to_dicts(uploaded_file)
     print(data)
+    for dict_line in data:
+        print(dict_line["Personal Email Address"])
+        print(json.dumps(dict_line))
+        mentor = Mentor(name=dict_line["First Name"] + " " + dict_line["Last Name"], email=dict_line["Personal Email Address"], other_attributes=json.dumps(dict_line))
+        mentor.save()
 
 
 def handle_return_mentor_files(uploaded_file):
-    convert_to_dicts(uploaded_file)
+    data = convert_to_dicts(uploaded_file)
     print(data)
