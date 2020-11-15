dadd3ddb15971fb1a89788ddd2fcede047cbc7f6
Ashley Connor <ash.connor@gmail.com>
2020-11-15 13:44:27 -0500

    Save Mentee on upload

--- matching/utils.py
+++ matching/utils.py
@@ -1,5 +1,8 @@
 import csv
 import io
+import re
+
+from .models import Mentee
 
 def convert_to_dicts(uploaded_file):
     file_bytes = uploaded_file.read().decode('utf-8')
@@ -8,8 +11,23 @@ def convert_to_dicts(uploaded_file):
 
 def handle_mentee_files(uploaded_file):
     data = convert_to_dicts(uploaded_file)
-    print(data)
 
+    for row in data:
+        name = f"{row['First Name']} {row['Last Name']}"
+        email = row["Email Address"]
+        attributes = {}
+
+        for key, value in row.items():
+            if re.search(r'\d', key):
+                numberless_key = re.sub(" \d+", "", key)
+                attr_array = attributes.get(numberless_key, [])
+                if value:
+                    attr_array.append(value)
+                attributes[numberless_key] = attr_array
+            else:
+                attributes[key] = value
+        mentee = Mentee(name=name, email=email, other_attributes=attributes)
+        mentee.save()
 
 def handle_mentor_files(uploaded_file):
     convert_to_dicts(uploaded_file)
