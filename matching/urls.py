120cfa159de85fc82cdf74df0ce9f5339cdb0343
Sandy Dai <sandydai@hotmail.com>
2020-11-15 16:18:19 -0500

    Syntax issues from merge/conflicting files

--- matching/urls.py
+++ matching/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("", views.index, name="index_cohort"),
     path("<int:cohort_id>", views.show, name="show_cohort"),
     path("upload", views.upload, name="upload"),
-    path("matching1", views.show_matching1, name="show_matching1")
+    path("matching1", views.show_matching1, name="show_matching1"),
     path("<int:cohort_id>/upload", views.upload, name="upload"),
 ]
