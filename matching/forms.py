0fa757194285fb7c49a82a4fb5780150ed0d9280
Ashley Connor <ash.connor@gmail.com>
2021-01-03 19:34:00 -0500

    Show form to create match

--- matching/forms.py
+++ matching/forms.py
@@ -1,8 +1,15 @@
 from django import forms
-from .models import Document
+from .models import Document, Match
 
 
 class UploadFileForm(forms.Form):
     model = Document
-    title = forms.CharField(max_length=50)
     file = forms.FileField()
+
+
+class MatchForm(forms.Form):
+    model = Match
+    mentor = forms.CharField(widget=forms.HiddenInput(), required=True)
+    mentee = forms.CharField(widget=forms.HiddenInput(), required=True)
+    # approver = forms.CharField(widget = forms.HiddenInput(), required=True)
+    # cohort = models.ForeignKey(Cohort, on_delete=models.CASCADE)
--- matching/urls.py
+++ matching/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.show, name="show_cohort"),
     path("upload", views.upload, name="upload"),
+    path("matches/new", views.new_match, name="new_match"),
     path("matches", views.show_matches, name="show_matches"),
     path("mentees", views.show_mentees, name="show_mentees"),
     path("mentors", views.show_mentors, name="show_mentors"),
