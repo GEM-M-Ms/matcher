b6b56feff11bb6c9e46464c1776442055bd7cad3
Ashley Connor <me@ashleyconnor.co.uk>
2021-01-25 21:58:03 -0500

    Allow deletion of matches
    - Form hides details of unselected mentors and mentees
    - Add CSV export of matches

--- matching/urls.py
+++ matching/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.show, name="show_cohort"),
+    path("", views.show_matches, name="show_matches"),
     path("matches/new", views.new_match, name="new_match"),
-    path("matches", views.show_matches, name="show_matches"),
+    path("matches/delete", views.delete_match, name="delete_match"),
     path("mentees", views.show_mentees, name="show_mentees"),
     path("mentors", views.show_mentors, name="show_mentors"),
-    path("sorted_mentors",views.sorted_mentors, name="sorted_mentors")
+    path("sorted_mentors", views.sorted_mentors, name="sorted_mentors")
+    path("export", views.export_matches, name="export_matches"),
 ]
